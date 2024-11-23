4d9e1cbac538a320729ec3bde7b4866c1f2c3399
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-23 14:07:13 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -6,7 +6,7 @@ image_ANGE1 = "ANGE1.jpg"
 img = Image.open(image_ANGE1)
 
 # Redimensionar la imagen a 113x152 píxeles (aproximadamente 3cm x 4cm)
-img_resized = img.resize((113, 152))
+img_resized = img.resize((125, 152))
 
 # Mostrar la imagen redimensionada
 st.image(img_resized)
