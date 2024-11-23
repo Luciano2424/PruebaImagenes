f46a87c49f006765325964055862d54299a1f9b0
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-23 13:42:56 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,3 +1,12 @@
 import streamlit as st
+from PIL import Image
 
-image_ANG1 = "ANG1.jpg"
+# Cargar la imagen
+image_ANGE1 = "ANGE1.jpg"
+img = Image.open(image_ANGE1)
+
+# Redimensionar la imagen a 113x152 píxeles (aproximadamente 3cm x 4cm)
+img_resized = img.resize((113, 152))
+
+# Mostrar la imagen redimensionada
+st.image(img_resized)
