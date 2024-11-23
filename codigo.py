6e9b43d1a800368c74c74902ab6d1abee242912a
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-23 20:54:45 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,10 +1,65 @@
 import streamlit as st
- 
-## Define las opciones
-options = ['Opción 1', 'Opción 2', 'Opción 3']
- 
-## Crea la caja de selección
-selected_option = st.selectbox('Elige una opción:', options)
- 
-## Muestra la opción seleccionada
-st.write('Seleccionaste:', selected_option)
+import pandas as pd
+from PIL import Image  
+
+df = pd.read_csv("valorant champions istanbul.csv")
+
+
+
+def mejor_rendimiento():
+    filas_seleccionadas = df.iloc[[5]]  
+    st.dataframe(filas_seleccionadas)
+
+def peor_rendimiento():
+    filas_seleccionadas = df.iloc[[19]]  
+    st.dataframe(filas_seleccionadas)
+
+def mas_kills():
+    filas_seleccionadas = df.iloc[[5]]  
+    st.dataframe(filas_seleccionadas)
+
+def mejor_rendimiento_por_equipo():
+    filas_seleccionadas = df.iloc[[3,5,10,17,22,25,32,35]]  
+    st.dataframe(filas_seleccionadas)
+
+# Ensure "page" exists in session state
+if "page" not in st.session_state:
+    st.session_state.page = "home"
+
+if st.session_state.page == "home":  
+    st.title("Datos que creemos te gustarán saber")
+
+    if st.button("Cuál fue el jugador con mejor rendimiento global del torneo"):
+        st.session_state.page = "mejor_rendimiento"
+    if st.button("Cuál fue el jugador con peor rendimiento global del torneo"):
+        st.session_state.page = "peor_rendimiento"
+    if st.button("Cuál fue el jugador con más kills?"):
+        st.session_state.page = "mas_kills"
+    if st.button("Cuáles fueron los jugadores con mejor rendimiento de cada equipo?"):
+        st.session_state.page = "mejor_rendimiento_por_equipo"
+
+elif st.session_state.page == "mejor_rendimiento":
+    st.title("Jugador con mejor rendimiento")
+    mejor_rendimiento()
+    if st.button("Volver a la página principal"):
+        st.session_state.page = "home"
+
+elif st.session_state.page == "peor_rendimiento":
+    st.title("Jugador con rendimiento mas bajo")
+    peor_rendimiento()
+    if st.button("Volver a la página principal"):
+        st.session_state.page = "home"
+
+elif st.session_state.page == "mas_kills":
+    st.title("Jugador con más bajas")
+    mas_kills()
+    if st.button("Volver a la página principal"):
+        st.session_state.page = "home"
+
+elif st.session_state.page == "mejor_rendimiento_por_equipo":
+    st.title("Jugadores con el mejor rendimiento por equipo")
+    mejor_rendimiento_por_equipo()
+
+
+    if st.button("Volver a la página principal"):
+        st.session_state.page = "home"
