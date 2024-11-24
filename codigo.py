9ad32d894b196f12ea29fe13a244a10f5fd4aefa
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-24 00:20:48 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -3,6 +3,10 @@ from PIL import Image
 
 image_LEV = "LEV.jpg"
 
+def display_image_with_caption(image_path, caption):
+    img_resized = resize_image(image_path)
+    st.image(img_resized)
+
 col1, col2, col3 = st.columns([1,2,3])
 with col2:
     st.subhead("Leviatan")
