16b1f64a85defa3536a00dc308161d29d3ed904a
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-24 05:29:19 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -42,34 +42,6 @@ def mejor_rendimiento_por_equipo():
     filas_seleccionadas = df.iloc[[3, 5, 10, 17, 22, 25, 32, 35]]  # Cambia el índice según tu CSV
     st.dataframe(filas_seleccionadas)
 
-def LOUD_page():
-    Loud_video = "https://www.youtube.com/watch?v=CSyGWW305M8&ab_channel=LOUDVALORANT"
-    st.video(Loud_video)
-
-def OPTC_page():
-    OPTC_video = "https://www.youtube.com/watch?v=tCa7ky1qeXw&ab_channel=TeamReyna"
-    st.video(OPTC_video)
-
-def FPX_page():
-    FPX_video = "https://www.youtube.com/watch?v=W0P7kGERa4o&ab_channel=TeamReyna"
-    st.video(FPX_video)
-
-def XSET_page():
-    XSET_video = "https://www.youtube.com/watch?v=3jsUSJUhlHg&ab_channel=TeamReyna"
-    st.video(XSET_video)
-
-def FNC_page():
-    FNC_video = "https://www.youtube.com/watch?v=Yr5X5CoEgKU&ab_channel=TeamReyna"
-    st.video(FNC_video)
-
-def TL_page():
-    TL_video = "https://www.youtube.com/watch?v=_aJSUUvTDMY&ab_channel=TeamReyna"
-    st.video(TL_video)
-
-def LEV_page():
-    LEV_video = "https://www.youtube.com/watch?v=cYaAr1mr0gY&ab_channel=LEVIATANVALORANT"
-    st.video(LEV_video)
-
 # Inicializar la página si no está definida
 if "page" not in st.session_state:
     st.session_state.page = "home"
@@ -115,27 +87,42 @@ def display_logo(image_path, name, width=100, height=100):
         st.image(img_resized)
         st.caption(name)
 
+# Función para mostrar los logos de los equipos con tamaño 300x300
+def imagenes_logos_300(image_path, width=300, height=300):
+    try:
+        img = Image.open(image_path)
+        img_resized = img.resize((width, height))  # Tamaño actualizado a 300x300
+        return img_resized
+    except Exception as e:
+        st.error(f"Error al cargar la imagen {image_path}: {e}")
+        return None
+
+# Función para mostrar la imagen con su nombre con tamaño 300x300
+def display_logo_300(image_path, name, width=300, height=300):  # Tamaño 300x300
+    img_resized = imagenes_logos_300(image_path, width, height)
+    if img_resized:
+        st.image(img_resized)
+        st.caption(name)
+
+
 # Mostrar el contenido de la página principal
 if st.session_state.page == "home":
     st.title("Análisis y Estadísticas del VCT Masters Reykjavik 2022: ¡Revive la Emoción del Torneo!")
     
     video_presentación_ = "https://www.youtube.com/watch?v=j2Z4qYJ3Jtc&ab_channel=VALORANTChampionsTour"
     st.video(video_presentación_)
-
-    col1, col2, col3 = st.columns([1, 3, 1])  
-    with col2:
-        st.subheader("Presentación de los equipos participantes del torneo")
+    st.subheader("Presentación de los equipos participantes del torneo")
 
     # Mostrar logos de los equipos
     col1, col2, col3 = st.columns([1, 1, 1]) 
     with col2:
-        display_logo(image_LOUD, "1 LOUD")
+        display_logo_300(image_LOUD, "1 LOUD")
 
     col4, col5, col6 = st.columns([1, 1, 1])  
     with col4:
-        display_logo(image_OPTC, "2 OPTC")
+        display_logo_300(image_OPTC, "2 OPTC")
     with col6:
-        display_logo(image_DRX, "3 DRX")
+        display_logo_300(image_DRX, "3 DRX")
 
     col7, col8, col9, col10, col11 = st.columns([1, 1, 1, 1, 1])  
     with col7:
@@ -149,28 +136,28 @@ if st.session_state.page == "home":
     with col11:
         display_logo(image_LEV, "8 Leviatán")
 
-   # Selección de datos adicionales
-page_selection = st.selectbox(
-    "Datos que creemos te gustarán saber",
-    ["Ház click para desplegar las opciones", 
-     "Cuál fue el jugador con mejor rendimiento global del torneo", 
-     "Cuál fue el jugador con peor rendimiento global del torneo", 
-     "Cuál fue el jugador con más kills?", 
-     "Cuáles fueron los jugadores con mejor rendimiento de cada equipo?"]
-)
+    # Selección de datos adicionales
+    page_selection = st.selectbox(
+        "Datos que creemos te gustarán saber",
+        ["Ház click para desplegar las opciones", 
+         "Cuál fue el jugador con mejor rendimiento global del torneo", 
+         "Cuál fue el jugador con peor rendimiento global del torneo", 
+         "Cuál fue el jugador con más kills?", 
+         "Cuáles fueron los jugadores con mejor rendimiento de cada equipo?"]
+    )
 
-# Redirigir a las páginas correspondientes según la selección del `selectbox`
-if page_selection == "Cuál fue el jugador con mejor rendimiento global del torneo":
-    st.session_state.page = "mejor_rendimiento"
+    # Redirigir a las páginas correspondientes según la selección del `selectbox`
+    if page_selection == "Cuál fue el jugador con mejor rendimiento global del torneo":
+        st.session_state.page = "mejor_rendimiento"
 
-elif page_selection == "Cuál fue el jugador con peor rendimiento global del torneo":
-    st.session_state.page = "peor_rendimiento"
+    elif page_selection == "Cuál fue el jugador con peor rendimiento global del torneo":
+        st.session_state.page = "peor_rendimiento"
 
-elif page_selection == "Cuál fue el jugador con más kills?":
-    st.session_state.page = "mas_kills"
+    elif page_selection == "Cuál fue el jugador con más kills?":
+        st.session_state.page = "mas_kills"
 
-elif page_selection == "Cuáles fueron los jugadores con mejor rendimiento de cada equipo?":
-    st.session_state.page = "mejor_rendimiento_por_equipo"
+    elif page_selection == "Cuáles fueron los jugadores con mejor rendimiento de cada equipo?":
+        st.session_state.page = "mejor_rendimiento_por_equipo"
 
 # Mostrar el contenido correspondiente
 if st.session_state.page == "mejor_rendimiento":
@@ -197,7 +184,8 @@ elif st.session_state.page == "mas_kills":
 elif st.session_state.page == "mejor_rendimiento_por_equipo":
     st.title("Jugadores con el mejor rendimiento por equipo")
     mejor_rendimiento_por_equipo()
-        # Primera fila de imágenes
+    
+    # Primera fila de imágenes
     col1, col2, col3, col4 = st.columns(4)
     with col1:
         display_image_with_caption(image_Less, "Less")
@@ -221,5 +209,59 @@ elif st.session_state.page == "mejor_rendimiento_por_equipo":
 
     if st.button("Volver a la página principal"):
         st.session_state.page = "home"
-    if st.button("Volver a la página principal"):
-        st.session_state.page = "home"
+
+st.subheader("¡Descubre el Poder de los Equipos!")
+st.text("Este gráfico muestra cómo se desempeñan los equipos en cuanto a su ratio de Kills/Deaths (K/D). Los equipos con el mejor desempeño suelen tener una mayor proporción de muertes por baja, lo que refleja una ejecución más eficiente en el juego. ¡Ve quién lidera el torneo en rendimiento!")
+plt.figure(figsize=(10, 6))
+kd = df.groupby('Team')['K/D'].mean().sort_values(ascending=False)
+plt.bar(kd.index, kd.values, color="purple")
+plt.xlabel('Equipo')
+plt.ylabel('Promedio K/D')
+plt.title('K/D promedio por equipo')
+plt.xticks(rotation="horizontal", ha='right')
+st.pyplot()  
+
+st.subheader("¿Quién es el Rey de las Bajas?")
+st.text("Este gráfico destaca la acumulación de kills de cada jugador a lo largo del torneo. Si estás buscando al jugador con más acción en el campo de batalla, aquí puedes ver quién se lleva la corona de las eliminaciones. ¡El jugador más letal está aquí!")
+plt.figure(figsize=(15, 6))
+kills = df.groupby('Player')['Kill'].mean().sort_values(ascending=False)
+plt.bar(kills.index, kills.values)
+plt.xlabel('Jugador')
+plt.ylabel('Kills')
+plt.title('Jugador con más kills')
+plt.xticks(rotation=45, ha='right')
+st.pyplot()  # Muestra el gráfico en Streamlit
+
+st.subheader("¡Las Muertes también Hablan!")
+st.text("A veces el precio del juego es alto, y este gráfico la cantidad de muertes de cada jugador. Aunque no es lo más positivo, saber quién lidera en esta categoría puede dar pistas sobre el estilo de juego o los desafíos a los que se enfrentan los jugadores. ¡Entérate de quiénes son los más golpeados en el torneo!")
+plt.figure(figsize=(15, 6))
+deaths = df.groupby('Player')['Death'].mean().sort_values(ascending=False)
+plt.bar(deaths.index, deaths.values, color="red")
+plt.xlabel('Jugador')
+plt.ylabel('Muertes')
+plt.title('Jugador con más muertes')
+plt.xticks(rotation=45, ha='right')
+st.pyplot()  # Muestra el gráfico en Streamlit
+
+st.subheader("Equipos Triunfadores: ¿Quién Tiene la Mayor Cantidad de Victorias?")
+st.text("El éxito no solo se mide en kills, sino también en victorias. Este gráfico revela a los equipos que más veces han salido victoriosos durante el torneo, mostrando quién tiene el control total en el campo de juego. ¡Descubre al equipo imbatible de esta edición!")
+plt.figure(figsize=(15, 6))
+victories = df.groupby('Team')['Rounds Win'].mean()
+plt.bar(victories.index, victories.values, color="green")
+plt.xlabel('Equipo')
+plt.ylabel('Victorias')
+plt.title('Equipo con más victorias')
+plt.xticks(rotation="horizontal", ha="center")
+st.pyplot()  # Muestra el gráfico en Streamlit
+
+    # Gráfico de Derrotas por equipo
+st.subheader("¿Quién Sufrió Más Derrotas?")
+st.text("Aunque cada derrota es una oportunidad para aprender, algunos equipos han tenido más dificultades que otros. Este gráfico muestra a los equipos con más derrotas, lo que podría reflejar puntos débiles o dificultades para adaptarse al estilo del torneo. ¡Descubre qué equipos tuvieron más trabajo durante la competencia!")
+plt.figure(figsize=(15, 6))
+defeats = df.groupby('Team')['Rounds Lose'].mean()
+plt.bar(defeats.index, defeats.values, color="cyan")
+plt.xlabel('Equipo')
+plt.ylabel('Derrotas')
+plt.title('Equipo con más derrotas')
+plt.xticks(rotation="horizontal", ha="center")
+st.pyplot()  # Muestra el gráfico en Streamlit
