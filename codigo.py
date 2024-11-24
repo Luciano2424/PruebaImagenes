8eb16470a397823d16643b906e21d7e0232b579b
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-24 00:11:13 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -17,17 +17,17 @@ def imagen_checkbox(image_path, label, key):
             st.session_state[key] = True
 
 # Definir un estado en la sesión para controlar el checkbox simulado
-if "imagen_ANG1_checked" not in st.session_state:
-    st.session_state["imagen_ANG1_checked"] = False
+if "imagen_ANGE1_checked" not in st.session_state:
+    st.session_state["imagen_ANGE1_checked"] = False
 
 # Título
 st.title("Simulando un Checkbox con la Imagen ANG1")
 
 # Mostrar la imagen como un "checkbox"
-imagen_checkbox("ANG1.jpg", "Imagen ANG1", "imagen_ANG1_checked")
+imagen_checkbox("ANGE1.jpg", "Imagen ANGE1", "imagen_ANGE1_checked")
 
 # Mostrar el estado del checkbox simulado
-if st.session_state["imagen_ANG1_checked"]:
-    st.write("La imagen ANG1 está seleccionada.")
+if st.session_state["imagen_ANGE1_checked"]:
+    st.write("La imagen ANGE1 está seleccionada.")
 else:
-    st.write("La imagen ANG1 no está seleccionada.")
+    st.write("La imagen ANGE1 no está seleccionada.")
