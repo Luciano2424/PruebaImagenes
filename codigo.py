268020d9202475c30f92bb258e048377b6ad7065
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-24 00:24:16 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 # Definir la función resize_image para cambiar el tamaño de la imagen
-def resize_image(image_path, width=180, height=180):
+def resize_image(image_path, width=100, height=100):
     img = Image.open(image_path)
     img_resized = img.resize((width, height))
     return img_resized
