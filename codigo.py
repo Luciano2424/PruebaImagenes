525bc298fafeeaeabb5183c69c9397f8cf5335a6
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-23 21:00:23 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -4,6 +4,17 @@ from PIL import Image
 
 df = pd.read_csv("valorant champions istanbul.csv")
 
+image_path_fondo = "Fondo.png"
+image_path_duelistas = "duelistas.jpg"
+image_yay = "yay.jpeg"
+image_Cryocells = "Cryocells.jpg"
+image_ANGE1 = "ANGE1.jpg"
+image_Derke = "Derke.jpg"
+image_MaKo = "MaKo.jpg"
+image_Scream = "Scream.jpg"
+image_kiNgg = "kiNgg.jpg"
+image_suygetsu = "suygetsu.jpg"
+image_Less = "Less.jpeg"
 
 
 def mejor_rendimiento():
@@ -26,40 +37,105 @@ def mejor_rendimiento_por_equipo():
 if "page" not in st.session_state:
     st.session_state.page = "home"
 
+def resize_image(image_path, width=130, height=152):
+    img = Image.open(image_path)  
+    img_resized = img.resize((width, height))  
+    return img_resized
+
+# Función para agregar leyenda con tipografía personalizada
+def display_image_with_caption(image_path, caption):
+    img_resized = resize_image(image_path)
+    st.image(img_resized)
+    st.markdown(
+        f"""
+        <style>
+        .caption {{
+            font-family: 'Arial Black', sans-serif;
+            font-size: 15px;
+            font-weight: bold;
+        }}
+        </style>
+        <p class="caption">{caption}</p>
+        """, unsafe_allow_html=True
+    )
+
 if st.session_state.page == "home":  
     st.title("Datos que creemos te gustarán saber")
 
-    if st.button("Cuál fue el jugador con mejor rendimiento global del torneo"):
+    # Replace buttons with selectbox
+    page_selection = st.selectbox(
+        "Selecciona una opción:",
+        ["Selecciona", "Cuál fue el jugador con mejor rendimiento global del torneo", 
+         "Cuál fue el jugador con peor rendimiento global del torneo", 
+         "Cuál fue el jugador con más kills?", 
+         "Cuáles fueron los jugadores con mejor rendimiento de cada equipo?"]
+    )
+
+    # Change page according to the selection in the selectbox
+    if page_selection == "Cuál fue el jugador con mejor rendimiento global del torneo":
         st.session_state.page = "mejor_rendimiento"
-    if st.button("Cuál fue el jugador con peor rendimiento global del torneo"):
+    elif page_selection == "Cuál fue el jugador con peor rendimiento global del torneo":
         st.session_state.page = "peor_rendimiento"
-    if st.button("Cuál fue el jugador con más kills?"):
+    elif page_selection == "Cuál fue el jugador con más kills?":
         st.session_state.page = "mas_kills"
-    if st.button("Cuáles fueron los jugadores con mejor rendimiento de cada equipo?"):
+    elif page_selection == "Cuáles fueron los jugadores con mejor rendimiento de cada equipo?":
         st.session_state.page = "mejor_rendimiento_por_equipo"
 
 elif st.session_state.page == "mejor_rendimiento":
     st.title("Jugador con mejor rendimiento")
     mejor_rendimiento()
-    if st.button("Volver a la página principal"):
+    display_image_with_caption(image_yay, "Presentación yay")
+    page_back = st.selectbox("Volver a la página principal", ["No", "Sí"])
+    if page_back == "Sí":
         st.session_state.page = "home"
 
 elif st.session_state.page == "peor_rendimiento":
     st.title("Jugador con rendimiento mas bajo")
     peor_rendimiento()
-    if st.button("Volver a la página principal"):
+    display_image_with_caption(image_ANGE1, "Presentación ANGE1")
+    page_back = st.selectbox("Volver a la página principal", ["No", "Sí"])
+    if page_back == "Sí":
         st.session_state.page = "home"
 
 elif st.session_state.page == "mas_kills":
     st.title("Jugador con más bajas")
     mas_kills()
-    if st.button("Volver a la página principal"):
+    display_image_with_caption(image_yay, "Presentación yay")
+    page_back = st.selectbox("Volver a la página principal", ["No", "Sí"])
+    if page_back == "Sí":
         st.session_state.page = "home"
 
 elif st.session_state.page == "mejor_rendimiento_por_equipo":
     st.title("Jugadores con el mejor rendimiento por equipo")
     mejor_rendimiento_por_equipo()
 
+    # Usando las columnas para mostrar las imágenes
+    col1, col2, col3, col4 = st.columns(4)  
+    with col1:
+        display_image_with_caption(image_Less, "Less Top 1")
 
-    if st.button("Volver a la página principal"):
+    with col2:
+        display_image_with_caption(image_yay, "yay Top 2")
+
+    with col3:
+        display_image_with_caption(image_MaKo, "MaKo Top 3")
+
+    with col4:
+        display_image_with_caption(image_suygetsu, "suygetsu Top 4")
+
+    col5, col6, col7, col8 = st.columns(4)
+    with col5:
+        display_image_with_caption(image_Cryocells, "Cryocells Top 5")
+
+    with col6:
+        display_image_with_caption(image_Derke, "Derke Top 6")
+
+    with col7:
+        display_image_with_caption(image_Scream, "Scream Top 7")
+
+    with col8:
+        display_image_with_caption(image_kiNgg, "kiNgg Top 8")
+
+    page_back = st.selectbox("Volver a la página principal", ["No", "Sí"])
+    if page_back == "Sí":
         st.session_state.page = "home"
