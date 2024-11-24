b00bbe1183cf2382c184708b558f24cbb7405bea
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-24 00:23:18 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,13 +1,25 @@
 import streamlit as st
 from PIL import Image
 
+# Definir la función resize_image para cambiar el tamaño de la imagen
+def resize_image(image_path, width=300, height=300):
+    img = Image.open(image_path)
+    img_resized = img.resize((width, height))
+    return img_resized
+
+# Ruta de la imagen
 image_LEV = "LEV.jpg"
 
+# Función para mostrar la imagen con el título/captura
 def display_image_with_caption(image_path, caption):
     img_resized = resize_image(image_path)
     st.image(img_resized)
+    st.markdown(f"**{caption}**", unsafe_allow_html=True)
 
-col1, col2, col3 = st.columns([1,2,3])
+# Diseño en columnas
+col1, col2, col3 = st.columns([1, 2, 3])
+
+# Mostrar la imagen en el centro de la columna 2
 with col2:
     st.subheader("Leviatan")
-    display_image_with_caption(image_LEV)
+    display_image_with_caption(image_LEV, "Equipo Leviatan")
