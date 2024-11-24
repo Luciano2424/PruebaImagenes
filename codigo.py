c48a41103bca2c807fd1d81b3f3a20ac13e7d01e
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-24 00:26:05 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from PIL import Image
 
-# Definir la función resize_image para cambiar el tamaño de la imagen
-def resize_image(image_path, width=100, height=100):
+# Definir la función para redimensionar la imagen
+def resize_image(image_path, width=200, height=200):
     img = Image.open(image_path)
     img_resized = img.resize((width, height))
     return img_resized
@@ -10,16 +10,28 @@ def resize_image(image_path, width=100, height=100):
 # Ruta de la imagen
 image_LEV = "LEV.jpg"
 
-# Función para mostrar la imagen con el título/captura
-def display_image_with_caption(image_path, caption):
+# Función para mostrar la imagen
+def display_image(image_path):
     img_resized = resize_image(image_path)
     st.image(img_resized)
-    st.markdown(f"**{caption}**", unsafe_allow_html=True)
 
-# Diseño en columnas
-col1, col2, col3 = st.columns([1, 2, 1])  # Columnas centradas: el medio más ancho
-
-# Mostrar la imagen en el centro de la columna 2
+# Diseño piramidal
+col1, col2, col3 = st.columns([1, 1, 1])  # Primera fila de 3 columnas
 with col2:
-    st.subheader("Leviatan")  # Subtítulo
-    display_image_with_caption(image_LEV, "Equipo Leviatan")  # Imagen y título
+    display_image(image_LEV)
+
+# Segunda fila (2 imágenes)
+col4, col5, col6 = st.columns([1, 1, 1])  # Segunda fila de 3 columnas
+with col4:
+    display_image(image_LEV)
+with col6:
+    display_image(image_LEV)
+
+# Tercera fila (3 imágenes)
+col7, col8, col9 = st.columns([1, 1, 1])  # Tercera fila de 3 columnas
+with col7:
+    display_image(image_LEV)
+with col8:
+    display_image(image_LEV)
+with col9:
+    display_image(image_LEV)
