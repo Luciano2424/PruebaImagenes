c56989219f9bfcd9585207c250d0c37db0284f32
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-24 00:47:00 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,35 +1,31 @@
 import streamlit as st
 from PIL import Image
 
-# Definir la función para redimensionar la imagen
+# Función para redimensionar la imagen
 def imagenes_logos(image_path, width=200, height=200):
-    img = Image.open(image_path)
-    img_resized = img.resize((width, height))
-    return img_resized
+    try:
+        img = Image.open(image_path)
+        img_resized = img.resize((width, height))
+        return img_resized
+    except Exception as e:
+        st.error(f"Error al cargar la imagen {image_path}: {e}")
+        return None
 
 # Rutas de las imágenes
 image_LEV = "LEV.jpg"
 image_DRX = "DRX.jpg"
 image_XSET = "XSET.jpg"
 image_FNC = "FNC.jpg"
-image_FPX = "FPX.jpg"  # Corregir el nombre de la variable
+image_FPX = "FPX.jpg"
 image_OPTC = "OPTC.jpg"
 image_LOUD = "LOUD.jpg"
-image_TL = "TL.jpg"  # Corregir el nombre de la variable
+image_TL = "TL.jpg"
 
 # Función para mostrar la imagen
 def display_logos(image_path):
     img_resized = imagenes_logos(image_path)
-    st.image(img_resized)
-
-def imagenes_logos2(image_path, width=100, height=100):
-    img = Image.open(image_path)
-    img_resized = img.resize((width, height))
-    return img_resized
-
-def display_58(image_path):
-    img_resized = imagenes_logos2(image_path)  # Corregir nombre de variable
-    st.image(img_resized)
+    if img_resized:
+        st.image(img_resized)
 
 # Pirámide logos top1-8
 col1, col2, col3 = st.columns([1, 1, 1]) 
@@ -44,10 +40,10 @@ with col6:
 
 col7, col8, col9, col10 = st.columns([1, 1, 1, 1])  
 with col7:
-    display_logos2(image_FPX)
+    display_logos(image_FPX)
 with col8:
-    display_logos2(image_XSET)
+    display_logos(image_XSET)
 with col9:
-    display_logos2(image_TL)
+    display_logos(image_TL)
 with col10:
-    display_logos2(image_LEV)
+    display_logos(image_LEV)
