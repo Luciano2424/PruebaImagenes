572d40b3a3bb0c5eb25e339ce0e8bab1bf097d8b
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-24 00:43:34 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -22,23 +22,34 @@ def display_logos(image_path):
     img_resized = imagenes_logos(image_path)
     st.image(img_resized)
 
-# Diseño piramidal
-col1, col2, col3 = st.columns([1, 1, 1])  # Primera fila de 3 columnas
+def imagenes_logos2((image_path, width=100, height=100):
+    img = Image.open(image_path)
+    img_resized = img.resize((width, height))
+    return img_resized
+    
+def display_58(image_path):
+    img_reasized = imagenes_logos2(image_path)
+    st.image(img_resized)
+
+
+#Piramide logos top1-8
+col1, col2, col3 = st.columns([1, 1, 1]) 
 with col2:
     display_logos(image_LOUD)
 
-# Segunda fila (2 imágenes)
-col4, col5, col6 = st.columns([1, 1, 1])  # Segunda fila de 3 columnas
+col4, col5, col6 = st.columns([1, 1, 1])  
 with col4:
     display_logos(image_OPTC)
 with col6:
-    display_logos(image_LEV)
+    display_logos(image_DRX)
 
-# Tercera fila (3 imágenes)
-col7, col8, col9 = st.columns([1, 1, 1])  # Tercera fila de 3 columnas
+col7, col8, col9, col10= st.columns([1, 1, 1, 1])  
 with col7:
-    display_logos(image_LEV)
+    display_logos2(image_FPX)
 with col8:
-    display_logos(image_LEV)
+    display_logos2(image_XSET)
 with col9:
-    display_logos(image_LEV)
+    display_logos2(image_TL)
+with col10:
+    display_logos2(image_LEV)
+
