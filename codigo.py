560a8191d3212c1d6dec775d1ab68541e3c2e4fd
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-24 00:20:12 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,19 +1,9 @@
 import streamlit as st
 from PIL import Image
 
-# Función para mostrar la imagen dentro de un 'expander' (showbox)
-def showbox_with_image(image_path, label):
-    # Cargar la imagen
-    img = Image.open(image_path)
-    
-    # Crear un expander (showbox) con el título de la imagen
-    with st.expander(label):
-        # Mostrar la imagen dentro del expander
-        st.image(img, use_column_width=True)
+image_LEV = "LEV.jpg"
 
-# Título de la aplicación
-st.title("Showbox con Imagen en Streamlit")
-
-# Llamada a la función para mostrar la imagen en un expander
-showbox_with_image("ANGE1.jpg", "Haz clic para ver la imagen ANGE1")
-
+col1, col2, col3 = st.columns([1,2,3])
+with col2:
+    st.subhead("Leviatan")
+    display_image_with_caption(image_Lev)
